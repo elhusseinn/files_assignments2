8edaa56e8537de1bf105c8e151f2bc05beb0a6de
elhusseinn <elhusseinabdalaziz1@gmail.com>
2021-12-21 23:04:04 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/files_assignment2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/files_assignment2.dir/main.cpp.obj"
+  "files_assignment2.exe"
+  "files_assignment2.exe.manifest"
+  "files_assignment2.pdb"
+  "libfiles_assignment2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/files_assignment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/files_assignment2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for files_assignment2.
+# This may be replaced when dependencies are built.
